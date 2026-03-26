79bb8fb036dc9f89935424fa96367b98b8a6b75f
Diego Pinheiro <126405977+DiegooPinheiro@users.noreply.github.com>
2026-03-26 17:21:33 -0300

    corrigindo erros de perfil do grupo

--- src/routes/conversationRoutes.js
+++ src/routes/conversationRoutes.js
@@ -1,15 +1,17 @@
-const express = require('express');
+﻿const express = require('express');
 const router = express.Router();
 const { 
   createConversation, 
   createGroup, 
   getUserConversations, 
+  getConversationById,
   deleteConversation 
 } = require('../controllers/conversationController');
 const { protect } = require('../middlewares/auth');
 
 router.post('/', protect, createConversation);
 router.post('/groups', protect, createGroup);
+router.get('/detail/:conversationId', protect, getConversationById);
 router.get('/:userId', protect, getUserConversations);
 router.delete('/:conversationId', protect, deleteConversation);
 
